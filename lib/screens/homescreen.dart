bc94b370c53d3bf8c618aba863fa4328ff4f5c1e
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-12-28 22:37:34 +0900

    ListViewとListTile追加

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -28,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Color(0xffFBF5F5),
       body: SingleChildScrollView(
+        physics: ScrollPhysics(),
         child: Padding(
           padding: const EdgeInsets.only(top: 55.0),
           child: Column(
@@ -48,7 +49,45 @@ class _HomeScreenState extends State<HomeScreen> {
                     buildTab("Height"),
                   ],
                 ),
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: ListView.builder(
+                    itemCount: 3,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 6.0),
+                        child: Card(
+                          elevation: 6.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: ListTile(
+                              leading: Image(
+                                width: 50,
+                                height: 50,
+                                image: AssetImage('images/weight.png'),
+                                fit: BoxFit.cover,
+                              ),
+                              title: Text(
+                                "65 kg",
+                                style: textStyle(27, Colors.black, FontWeight.w600),
+                              ),
+                              trailing: Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                                size: 28,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
             ],
           ),
         ),
